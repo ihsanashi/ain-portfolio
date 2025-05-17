7b7bbd7cf776b5c36be4c3009307c5d9fb593090
Ahmad Ihsan <ihsanashi@gmail.com>
2025-05-17 21:22:24 +0800

    feat: implement 2 column gallery component

--- src/blocks/Gallery/TwoColumns/index.tsx
+++ src/blocks/Gallery/TwoColumns/index.tsx
@@ -1,7 +1,33 @@
-'use client';
-
 import { Gallery } from '@/payload-types';
+import { CMSLink } from '@/components/Link';
+import { Media } from '@/components/Media';
+import RichText from '@/components/RichText';
 
-export const TwoColumns: React.FC<Gallery> = () => {
-  return <h1>Two columns</h1>;
+export const TwoColumns: React.FC<Gallery> = (props) => {
+  const { content, mediaPlacement } = props;
+  const item = content[0];
+
+  return (
+    <section className="grid grid-cols-1 place-items-center gap-5 px-5 md:grid-cols-2 md:px-10 lg:px-20">
+      {/* media */}
+      <div
+        className={`${mediaPlacement === 'left' ? 'order-first' : 'order-last'}`}
+      >
+        <Media
+          imgClassName="object-cover h-[320px] md:h-[380px] lg:h-[460px]"
+          htmlElement="article"
+          resource={item?.media}
+        />
+      </div>
+      {/* text content */}
+      <div className="flex flex-col gap-y-5">
+        {item?.richText && <RichText data={item.richText} />}
+        {item?.link && (
+          <div className="container mx-auto">
+            <CMSLink {...item.link} />
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
