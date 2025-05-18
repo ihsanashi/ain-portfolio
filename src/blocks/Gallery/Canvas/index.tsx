7f0e91e7954b9be4e0d52bb3d0262e10d6969bd2
Ahmad Ihsan <ihsanashi@gmail.com>
2025-05-18 16:01:57 +0800

    feat: implement canvas gallery type

--- src/blocks/Gallery/Canvas/index.tsx
+++ src/blocks/Gallery/Canvas/index.tsx
@@ -1,5 +1,32 @@
+import { CMSLink } from '@/components/Link';
 import { Gallery } from '@/payload-types';
+import { Media } from '@/components/Media';
+import RichText from '@/components/RichText';
 
-export const Canvas: React.FC<Gallery> = () => {
-  return <h1>Canvas</h1>;
+export const Canvas: React.FC<Gallery> = (props) => {
+  const { content } = props;
+  const item = content[0];
+
+  return (
+    <section className="relative">
+      <Media
+        className="h-[800px] w-dvw"
+        imgClassName="h-full w-full"
+        resource={item?.media}
+      />
+
+      {(item?.richText || item?.link) && (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+          {item.richText && (
+            <RichText
+              className="mb-4"
+              data={item.richText}
+              enableGutter={false}
+            />
+          )}
+          {item.link?.label && <CMSLink {...item.link} />}
+        </div>
+      )}
+    </section>
+  );
 };
