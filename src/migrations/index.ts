c326c4b35f6b9ce2016fec55ec21d778e51e5306
Ahmad Ihsan <ihsanashi@gmail.com>
2025-05-23 14:54:48 +0800

    build: ran migration create command ffs

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250523_065006 from './20250523_065006';
+
+export const migrations = [
+  {
+    up: migration_20250523_065006.up,
+    down: migration_20250523_065006.down,
+    name: '20250523_065006'
+  },
+];
