c4767eb11310d9d26e9875d6cb5d502570ff5ca2
Ahmad Ihsan <ihsanashi@gmail.com>
2025-05-24 09:16:00 +0800

    build: create migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250523_065006 from './20250523_065006';
+import * as migration_20250524_010829 from './20250524_010829';
 
 export const migrations = [
   {
     up: migration_20250523_065006.up,
     down: migration_20250523_065006.down,
-    name: '20250523_065006'
+    name: '20250523_065006',
+  },
+  {
+    up: migration_20250524_010829.up,
+    down: migration_20250524_010829.down,
+    name: '20250524_010829'
   },
 ];
